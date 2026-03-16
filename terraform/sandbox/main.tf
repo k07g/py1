66c04cd4de985982350fbbe4e3ef2b72ea37412d
k07g <korenaga.makoto@ea-sys.jp>
2026-03-16 21:50:04 +0900

    Lambdaの作成

--- terraform/sandbox/main.tf
+++ terraform/sandbox/main.tf
@@ -6,3 +6,123 @@ resource "aws_ecr_repository" "main" {
     scan_on_push = true
   }
 }
+
+# -------------------------------------------------------------------
+# DynamoDB
+# -------------------------------------------------------------------
+
+resource "aws_dynamodb_table" "books" {
+  name         = "books"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+# -------------------------------------------------------------------
+# IAM Role for Lambda
+# -------------------------------------------------------------------
+
+resource "aws_iam_role" "lambda" {
+  name = "py1-lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb" {
+  name = "dynamodb"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Scan",
+      ]
+      Resource = aws_dynamodb_table.books.arn
+    }]
+  })
+}
+
+# -------------------------------------------------------------------
+# Lambda
+# -------------------------------------------------------------------
+
+resource "aws_lambda_function" "main" {
+  function_name = "py1"
+  role          = aws_iam_role.lambda.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.main.repository_url}:latest"
+  timeout       = 30
+  memory_size   = 512
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.books.name
+    }
+  }
+}
+
+# -------------------------------------------------------------------
+# API Gateway (HTTP API)
+# -------------------------------------------------------------------
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "py1"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.main.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.main.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "prod" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+# -------------------------------------------------------------------
+# Outputs
+# -------------------------------------------------------------------
+
+output "api_endpoint" {
+  value = "${aws_apigatewayv2_stage.prod.invoke_url}"
+}
